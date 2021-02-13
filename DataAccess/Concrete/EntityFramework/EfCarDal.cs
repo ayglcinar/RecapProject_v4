a74ecf92c6c5c13b6df724d67eb6cffe8d620f8b
ayglcinar <60033267+ayglcinar@users.noreply.github.com>
2021-02-13 20:20:58 +0300

    Add files via upload

---
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCarDal : EfEntityRepositoryBase<Car, RentACarContext>, ICarDal
+    {
+        public List<CarDetailDto> GetCarDetails()
+        {
+            using(RentACarContext context = new RentACarContext())
+            {
+                var result = from c in context.Cars
+                             join b in context.Brands
+                             on c.BrandID equals b.BrandID
+                             join r in context.Colors
+                             on c.ColorID equals r.ColorID
+                    select new CarDetailDto
+                    {
+                        CarName = c.CarName,
+                        BrandName = b.BrandName,
+                        ColorName = r.ColorName,
+                        DailyPrice = c.DailyPrice
+                    };
+                return result.ToList();
+
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Core.DataAccess;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{ ID=1, BrandID=1, ColorID=1, ModelYear= "2015", DailyPrice = 290, Description = "Benzin"},
+                new Car{ ID=2, BrandID=1, ColorID=2, ModelYear= "2018", DailyPrice = 450, Description = "Dizel"},
+                new Car{ ID=3, BrandID=2, ColorID=1, ModelYear= "2015", DailyPrice = 590, Description = "Benzin"},
+                new Car{ ID=4, BrandID=2, ColorID=2, ModelYear= "2018", DailyPrice = 590, Description = "Benzin"},
+                new Car{ ID=5, BrandID=2, ColorID=3, ModelYear= "2020", DailyPrice = 590, Description = "Dizel"}
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.ID == car.ID);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<CarDetailDto> GetCarDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GeyById(int BrandID)
+        {
+            return _cars.Where(c => c.BrandID == BrandID).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.ID == car.ID);
+            carToUpdate.BrandID = car.BrandID;
+            carToUpdate.ColorID = car.ColorID;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+        }
+
+        Car IEntityRepository<Car>.Get(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<Car> IEntityRepository<Car>.GetAll(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
